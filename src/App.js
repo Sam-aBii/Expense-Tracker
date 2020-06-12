c0c4165c3340bec3131d57e02a55d59facc741a7
Sam-aBii <samabii786@gmail.com>
2020-06-12 22:54:37 +0500

    Static UI Done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Header } from "./components/Header";
-import { Balance }from "./components/Balance";
-import { IncomeExpense }from "./components/IncomeExpense";
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
 
 function App() {
   return (
     <>
-     <Header /> 
-     <div className="container">
-       <Balance />
-       <IncomeExpense />
-       <TransactionList />
-       <AddTransaction />
-     </div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </>
   );
 }
