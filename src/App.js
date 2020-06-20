4990e35852d8bf34bd90d793cdebe6e8d2f9fa94
Sam-aBii <samabii786@gmail.com>
2020-06-20 14:32:26 +0500

    App complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
-import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
-import { IncomeExpense } from "./components/IncomeExpense";
+import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
 
+import { GlobalProvider } from "./context/GlobalState";
+
+import "./App.css";
+
 function App() {
   return (
-    <>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
-        <IncomeExpense />
+        <IncomeExpenses />
         <TransactionList />
         <AddTransaction />
       </div>
-    </>
+    </GlobalProvider>
   );
 }
 
